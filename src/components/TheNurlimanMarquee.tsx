80d7385c7189f4947eabf5c23e99964b9eb3606a
Fachri Alevins <96483561+fachriagustian@users.noreply.github.com>
2024-10-02 21:03:24 +0700

    Add files via upload

---
+++ src/components/TheNurlimanMarquee.tsx
@@ -0,0 +1,12 @@
+import TheMarquee from "@/components/TheMarquee";
+
+export default function TheNurlimanMarquee() {
+  return (
+    <TheMarquee clone>
+      <div class="inline-flex font-bold font-transducer-extended text-[10px]">
+        <span>NURLIMAN DIARA ARIA</span>
+        <span class="mx-2.5">&#8226;</span>
+      </div>
+    </TheMarquee>
+  );
+}
---
+++ src/data/me.ts
@@ -0,0 +1,266 @@
+import dedent from "dedent";
+
+export const me = {
+  name: "Fachri Agustian Suhliandana",
+  titles: [
+    "Web Developer",
+    "Frontend Developer",
+    "Backend Developer",
+    "JavaScript/TypeScript Programmer",
+    "DevOps",
+    "Linux Administrator",
+    "UI/UX Designer",
+    "Graphic Designer",
+    "IT Busness Analyst",
+  ],
+  address: "Cibiru, Bandung",
+  email: "Fachri.AgustianSuhliandana@gmail.com",
+  phone: {
+    display: "+62 085923261431",
+    value: 085923261431,
+  },
+  experiences: [
+    {
+      period: "Jun 22 - Present ",
+      company: "PT. Knitto Tekstil Indonesia",
+      title: "IT  Business",
+      description: dedent`
+        As the Lead Frontend & DevOps Engineer at PT. Knitto Tekstil Indonesia, I spearheaded the integration of cutting-edge technologies to optimize software development processes and enhance overall system efficiency. In this pivotal role, I undertook the following responsibilities:
+        <br /><br />
+        **Frontend Development:**
+        - Led a team of frontend developers in designing and implementing responsive, user-friendly interfaces for web applications, ensuring seamless user experiences.
+        - Developed and maintained efficient, reusable, and scalable code using modern JavaScript frameworks such as React.js and Next.js.
+        - Collaborated closely with UI/UX designers to translate design mockups into functional frontend components, ensuring alignment with the overall project vision.
+
+        **DevOps Integration:**
+        - Pioneered the implementation of DevOps best practices to streamline the software development lifecycle, resulting in a significant reduction in deployment time and enhanced collaboration between development and operations teams.
+        - Automated deployment processes using tools such as Docker and Kubernetes, leading to a more reliable and scalable infrastructure.
+        - Implemented continuous integration and continuous delivery (CI/CD) pipelines, facilitating rapid and reliable software releases.
+
+        **Infrastructure Management:**
+        - Orchestrated cloud infrastructure deployment and management, leveraging services from providers such as Google Cloud Platform or Vercel, ensuring optimal performance, scalability, and cost-effectiveness.
+        - Implemented monitoring and alerting systems to proactively identify and address potential issues, minimizing downtime and improving system reliability.
+
+        **Team Leadership:**
+        - Provided mentorship and guidance to the frontend and DevOps teams, fostering a collaborative and innovative work environment.
+        - Conducted regular code reviews to ensure code quality, adherence to best practices, and knowledge sharing among team members.
+        - Collaborated with cross-functional teams, including product managers and backend engineers, to deliver integrated solutions that met both technical and business requirements.
+      `,
+    },
+    {
+      period: "Jun 22 - Present ",
+      company: "PT. Knitto Tekstil Indonesia",
+      title: "IT  Business",
+      period: "Jun 22 - Present ",
+      company: "PT. Knitto Tekstil Indonesia",
+      title: "IT  Business",
+      description: dedent`
+        As an IT Programmer/Web Developer at PT. Knitto Tekstil Indonesia, I played a pivotal role in advancing the company's digital presence and internal workflow through the development and maintenance of various applications. Key responsibilities and achievements include:
+        <br /><br />
+        **Main Website Development:**
+        - Spearheaded the design and development of the company's main website, focusing on creating an intuitive and visually appealing online platform.
+        - Implemented responsive web design principles to ensure seamless user experiences across various devices, contributing to a 20% increase in website traffic.
+        - Collaborated closely with cross-functional teams to gather requirements and translate them into functional features, enhancing the overall online brand presence.
+
+        **Online Ordering Web App:**
+        - Led the development of a user-friendly online ordering web application, streamlining the customer ordering process.
+        - Integrated secure payment gateways and implemented robust authentication mechanisms to ensure the confidentiality and integrity of customer data.
+        - Improved the app's performance through optimization techniques, resulting in a 15% decrease in page load times and increased customer satisfaction.
+
+        **Internal Apps Development:**
+        - Developed and maintained several internal applications to enhance operational efficiency.
+        - Implemented custom features based on user feedback, resulting in a 25% reduction in manual data entry and improved overall workflow automation.
+
+        **Technological Stack:**
+        - Utilized a stack comprising HTML, CSS, JavaScript, and various frontend frameworks to create dynamic and engaging user interfaces.
+        - Integrated backend technologies such as Node.js and Express to build robust server-side components for web applications.
+        - Pioneered the implementation of version control using Git to manage codebase changes efficiently and facilitate collaboration among the development team.
+      `,
+    },
+    {
+      period: "Jun 22 - Present ",
+      company: "PT. Knitto Tekstil Indonesia",
+      title: "IT  Business",
+      description: dedent`Designing packaging of product,
+        Designing products catalog booklet,
+        Designing brochures,
+        posters and other campaign properties,
+        Designing website and build it,
+        Maintains web environment by identifying system requirements,
+        installing upgrades and monitoring system performance,
+        Create and maintain mobile applications called “Salesman Tracking”.`,
+    },
+    {
+      period: "Jun 22 - Present ",
+      company: "PT. Knitto Tekstil Indonesia",
+      title: "IT  Business",
+      description: dedent`Establishes and maintains network performance,
+        Builds network configurations and connections,
+        Maximizes performance by troubleshooting network problems and outages and scheduling upgrades,
+        Establishes networking environment by designing system configuration and directing system installation,
+        Accomplishes information systems and organization mission by completing related results as needed.`,
+    },
+  ],
+  educations: [
+    {
+      period: "2015 - 2019",
+      University: "University Of Computer Indonesia",
+      field: "System Analysis",
+      description: dedent`Learn how to design and build computer and communication networks.
+        Some topics covered include:
+          Operating systems,
+          Database administration,
+          Networking, Security,
+          Routing and cabling.`,
+    },
+  ],
+  technicalSkills: [
+    { name: "Kubernetes", value: 0.88 },
+    { name: "Docker", value: 0.95 },
+    { name: "JavaScript/TypeScript", value: 0.99 },
+    { name: "Rust", value: 0.85 },
+    { name: "React/Preact", value: 0.99 },
+    { name: "React Native", value: 0.98 },
+    { name: "Astro", value: 0.95 },
+    { name: "Solid.js", value: 0.95 },
+    { name: "Svelte", value: 0.92 },
+    { name: "SvelteKit", value: 0.92 },
+    { name: "Vue.js", value: 0.9 },
+    { name: "Nuxt", value: 0.9 },
+    { name: "Next.js", value: 0.96 },
+    { name: "Node.js", value: 0.93 },
+    { name: "HTML/CSS", value: 0.98 },
+    { name: "Git/Github", value: 0.95 },
+    { name: "Go", value: 0.9 },
+    { name: "Java/Kotlin", value: 0.8 },
+    { name: "Android Development", value: 0.8 },
+  ],
+  otherSkills: [
+    { name: "Adobe Illustrator", value: 0.93 },
+    { name: "Adobe XD", value: 0.91 },
+    { name: "Figma", value: 0.8 },
+    { name: "Github Actions", value: 0.9 },
+    { name: "Google Cloud Platform", value: 0.9 },
+    { name: "Vercel", value: 0.9 },
+    { name: "npm", value: 0.9 },
+    { name: "Computer Networking", value: 0.8 },
+    { name: "Linux", value: 0.9 },
+    { name: "Nginx", value: 0.9 },
+    { name: "HAProxy", value: 0.8 },
+    { name: "Keepalived", value: 0.8 },
+    { name: "Wordpress", value: 0.9 },
+  ],
+  knowledges: [
+    "Containerization",
+    "Clean Code",
+    "Cloud Computing",
+    "CI/CD",
+    "DevOps",
+    "Serverless",
+    "Edge Computing",
+    "Jamstack",
+    "SEO",
+    "Kubernetes",
+    "Monorepo",
+    "Microservice",
+    "REST API",
+    "GraphQL",
+    "gRPC",
+    "JWT",
+    "OAuth",
+    "PASETO",
+    "Preact",
+    "SolidJS",
+    "Svelte",
+    "UI/UX",
+    "TailwindCSS",
+    "Design System",
+    "TDD",
+    "Vite",
+    "Vitest",
+    "Cypress",
+    "WebAssembly",
+    "Webpack",
+    "Rust",
+  ],
+  certificates: [
+    {
+      name: "Google Cloud Platform Fundamentals: Core Infrastructure",
+      organization: {
+        name: "Google",
+        logoUrl: "/assets/google-G-logo.svg",
+        logoDarkUrl: "/assets/google-G-logo.svg",
+      },
+      date: "November 2021",
+      credential: {
+        id: "E-Certificate",
+        url: "/assets/e-certificate-gcp-core-infrastructure.pdf",
+      },
+    },
+    {
+      name: "The Git & Github Bootcamp",
+      organization: {
+        name: "Udemy",
+        logoUrl: "/assets/udemy-logo.svg",
+        logoDarkUrl: "/assets/udemy-logo-dark.svg",
+      },
+      date: "September 2021",
+      credential: {
+        id: "UC-fc8713f1-cc7e-4dce-b002-15dc2c356ede",
+        url: "https://www.udemy.com/certificate/UC-fc8713f1-cc7e-4dce-b002-15dc2c356ede",
+      },
+    },
+    {
+      name: "Go: The Complete Developer's Guide (Golang)",
+      organization: {
+        name: "Udemy",
+        logoUrl: "/assets/udemy-logo.svg",
+        logoDarkUrl: "/assets/udemy-logo-dark.svg",
+      },
+      date: "August 2021",
+      credential: {
+        id: "UC-d8cc5a83-e2dd-49cb-bf1d-00644570c7c1",
+        url: "https://www.udemy.com/certificate/UC-d8cc5a83-e2dd-49cb-bf1d-00644570c7c1",
+      },
+    },
+    {
+      name: "Mastering Multithreading Programming with Go (Golang)",
+      organization: {
+        name: "Udemy",
+        logoUrl: "/assets/udemy-logo.svg",
+        logoDarkUrl: "/assets/udemy-logo-dark.svg",
+      },
+      date: "August 2021",
+      credential: {
+        id: "UC-e3365e37-87b5-4d53-a1e3-4ff6353d4718",
+        url: "https://www.udemy.com/certificate/UC-e3365e37-87b5-4d53-a1e3-4ff6353d4718",
+      },
+    },
+    {
+      name: "Web Authentication, Encryption, JWT, HMAC, & OAuth With Go",
+      organization: {
+        name: "Udemy",
+        logoUrl: "/assets/udemy-logo.svg",
+        logoDarkUrl: "/assets/udemy-logo-dark.svg",
+      },
+      date: "August 2021",
+      credential: {
+        id: "UC-f0ecf5e1-f436-4b7d-b181-82a615295bf2",
+        url: "https://www.udemy.com/certificate/UC-f0ecf5e1-f436-4b7d-b181-82a615295bf2",
+      },
+    },
+    {
+      name: "gRPC [Golang] Master Class: Build Modern API & Microservices",
+      organization: {
+        name: "Udemy",
+        logoUrl: "/assets/udemy-logo.svg",
+        logoDarkUrl: "/assets/udemy-logo-dark.svg",
+      },
+      date: "August 2021",
+      credential: {
+        id: "UC-addc957e-0609-4382-87bd-7c85814bcb19",
+        url: "https://www.udemy.com/certificate/UC-addc957e-0609-4382-87bd-7c85814bcb19",
+      },
+    },
+  ],
+} as const;
